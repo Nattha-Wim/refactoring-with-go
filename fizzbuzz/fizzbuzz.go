dbb43a205980503979d64546b8df6d06c399d589
Mook <natthaporn.wim@gmail.com>
2022-12-20 14:17:33 +0700

    refactoring

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -1,15 +1,11 @@
 package fizzbuzz
 
+import "fmt"
+
 func FizzBuzz(num int) string {
 	if num == 3 {
 		return "Fizz"
 	}
-	if num == 2 {
-		return "2"
-	}
-	if num == 4 {
-		return "4"
-	}
-
-	return "1"
+	// convert int to string
+	return fmt.Sprintf("%d", num)
 }
