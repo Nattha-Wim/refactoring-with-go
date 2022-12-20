542b0fb26442c0084c3b9be0f02dc31f6eeeb728
Mook <natthaporn.wim@gmail.com>
2022-12-20 15:06:19 +0700

    refactor is done

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -3,15 +3,30 @@ package fizzbuzz
 import "fmt"
 
 func FizzBuzz(num int) string {
-	if num%15 == 0 {
+	if isFizzBuzz(num) {
 		return "FizzBuzz"
 	}
-	if num%3 == 0 {
+	if isFizz(num) {
 		return "Fizz"
 	}
-	if num%5 == 0 {
+	if isBuzz(num) {
 		return "Buzz"
 	}
 	// convert int to string
 	return fmt.Sprintf("%d", num)
 }
+
+func isFizz(num int) bool {
+	return num%3 == 0
+}
+
+func isBuzz(num int) bool {
+	return num%5 == 0
+}
+
+func isFizzBuzz(num int) bool {
+	return num%15 == 0
+}
+
+// go test -coverprofile c.out
+// go tool cover -html=c.out
