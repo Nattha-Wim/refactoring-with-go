f91d9d97a7a579a0be0dc0a5a550e5239c8ce8e2
Mook <natthaporn.wim@gmail.com>
2022-12-21 14:21:19 +0700

    use go idiom 2

--- rental/movie.go
+++ rental/movie.go
@@ -1,8 +1,11 @@
 package rental
 
-const CHILDRENS = 2
-const NEW_RELEASE = 1
-const REGULAR = 0
+const (
+	_ = iota
+	CHILDRENS
+	NEW_RELEASE
+	REGULAR
+)
 
 type Movie struct {
 	title     string
@@ -15,12 +18,9 @@ func NewMovie(title string, priceCode int) (rcvr Movie) {
 		priceCode: priceCode,
 	}
 }
-func (rcvr Movie) GetPriceCode() int {
+func (rcvr Movie) PriceCode() int {
 	return rcvr.priceCode
 }
-func (rcvr Movie) GetTitle() string {
+func (rcvr Movie) Title() string {
 	return rcvr.title
-}
-func (rcvr Movie) SetPriceCode(arg int) {
-	rcvr.priceCode = arg
 }
