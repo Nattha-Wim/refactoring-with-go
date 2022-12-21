d602736fc9b24e79ba8e32643fb4a4e16cba84ba
Mook <natthaporn.wim@gmail.com>
2022-12-21 15:39:19 +0700

    extract charge 3 type

--- rental/customer.go
+++ rental/customer.go
@@ -20,21 +20,39 @@ func (rcvr *Customer) Name() string {
 	return rcvr.name
 }
 
+func RegularCharge(r Rental) float64 {
+	result := 0.0
+	result += 2
+	if r.DaysRented() > 2 {
+		result += float64(r.DaysRented()-2) * 1.5
+	}
+	return result
+}
+
+func NewReleaseCharge(r Rental) float64 {
+	result := 0.0
+	result += float64(r.DaysRented()) * 3.0
+	return result
+}
+
+func ChildrensCharge(r Rental) float64 {
+	result := 0.0
+	result += 1.5
+	if r.DaysRented() > 3 {
+		result += float64(r.DaysRented()-3) * 1.5
+	}
+	return result
+}
+
 func (r Rental) Charge() float64 { // Charge belong to Rental so we can convert Charge to Rental's methor
 	result := 0.0
 	switch r.Movie().PriceCode() {
 	case REGULAR:
-		result += 2
-		if r.DaysRented() > 2 {
-			result += float64(r.DaysRented()-2) * 1.5
-		}
+		return RegularCharge(r)
 	case NEW_RELEASE:
-		result += float64(r.DaysRented()) * 3.0
+		return NewReleaseCharge(r)
 	case CHILDRENS:
-		result += 1.5
-		if r.DaysRented() > 3 {
-			result += float64(r.DaysRented()-3) * 1.5
-		}
+		return ChildrensCharge(r)
 	}
 	return result
 }
@@ -47,19 +65,35 @@ func GetPoint(r Rental) int {
 	return 1
 }
 
-func (rcvr Customer) Statement() string {
-	totalAmount := 0.0
-	frequentRenterPoints := 0
-	result := fmt.Sprintf("Rental Record for %v\n", rcvr.Name())
-	for _, r := range rcvr.rentals {
-		thisAmount := r.Charge()
-
-		frequentRenterPoints += GetPoint(r)
-
-		result += fmt.Sprintf("\t%v\t%.1f\n", r.Movie().Title(), thisAmount)
-		totalAmount += thisAmount
+func GetTotalAmount(rentals []Rental) float64 {
+	result := 0.0
+	for _, r := range rentals {
+		result += r.Charge()
 	}
+	return result
+}
+
+func GetTotalPoint(rentals []Rental) int {
+	// frequentRenterPoints := 0
+	points := 0
+	for _, r := range rentals {
+		points += GetPoint(r)
+	}
+	return points
+}
+
+func (c Customer) Statement() string {
+	points := GetTotalPoint(c.rentals)
+
+	totalAmount := GetTotalAmount(c.rentals)
+
+	result := fmt.Sprintf("Rental Record for %v\n", c.Name())
+
+	for _, r := range c.rentals {
+		result += fmt.Sprintf("\t%v\t%.1f\n", r.Movie().Title(), r.Charge())
+	}
+
 	result += fmt.Sprintf("Amount owed is %.1f\n", totalAmount)
-	result += fmt.Sprintf("You earned %v frequent renter points", frequentRenterPoints)
+	result += fmt.Sprintf("You earned %v frequent renter points", points)
 	return result
 }
